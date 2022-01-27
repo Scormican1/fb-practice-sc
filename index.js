5929c6bda7275643a64f52396509fd0b0b579ede
Sara Cormican <S_Comican@yahoo.com>
2022-01-27 10:58:44 -0500

    imported resturants.json

---
+++ index.js
@@ -0,0 +1,10 @@
+// import our reasturants
+const restaurants = require('./resturants.json');
+
+console.log(restaurants[0].name)
+
+// connect to firestore
+
+// create a collection called "resturants"
+
+// add each resturants
